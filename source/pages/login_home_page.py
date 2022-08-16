61027129e530b1c6ea491397ffc53604d5abead5
oleksii.k.khokhlov <oleksii.k.khokhlov@globallogic.com>
2022-08-16 22:42:05 +0300

    refactoring

--- source/pages/login_home_page.py
+++ source/pages/login_home_page.py
@@ -9,14 +9,14 @@ class Login_Home_Page:
     def __init__(self, driver):
         self.driver = driver
 
-    def do_some_with_mail(self, email1: str):
+    def send_email(self, email1: str):
         email = self.driver.find_element(By.ID, self.__EMAIL_ID)
         email.send_keys(email1)
 
-    def do_some_with_pass(self, pass1: str):
+    def send_password(self, pass1: str):
         password = self.driver.find_element(By.ID, self.__PASSWORD_ID)
         password.send_keys(pass1)
 
-    def do_some_with_button(self):
+    def click_button(self):
         button = self.driver.find_element(By.XPATH, self.__SIGN_IN_BUTTON)
         button.click()
--- source/pages/register_home_page.py
+++ source/pages/register_home_page.py
@@ -13,25 +13,25 @@ class Register_Home_Page:
     def __init__(self, driver):
         self.driver = driver
 
-    def do_some_with_name(self, name1: str):
+    def send_name(self, name1: str):
         name = self.driver.find_element(By.ID, self.__First_Name_ID)
         name.send_keys(name1)
 
-    def do_some_with_last_name(self, name2: str):
+    def send_last_name(self, name2: str):
         last_name = self.driver.find_element(By.ID, self.__Last_Name_ID)
         last_name.send_keys(name2)
 
-    def do_some_with_email(self, email1: str):
+    def send_email(self, email1: str):
         email = self.driver.find_element(By.ID, self.__EMAIL_ID)
         email.send_keys(email1)
-    def do_some_with_pass(self,password1:str):
+    def send_password(self,password1:str):
         password = self.driver.find_element(By.ID, self.__PASSWORD_ID)
         password.send_keys(password1)
 
-    def do_some_with_checkbox(self):
+    def click_checkbox(self):
         checkbox = self.driver.find_element(By.CLASS_NAME, self.__CHECKBOX)
         checkbox.click()
 
-    def do_some_with_button(self):
+    def click_button(self):
         button = self.driver.find_element(By.XPATH, self.__SIGN_UP_BUTTON)
         button.click()
